feb1002bdf39d5045d3a170e25fd3b6d203eda32
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2024-12-10 12:37:56 +0100

    First commit

---
+++ src/main/java/com/torresj/infosas/InfosasApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.infosas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfosasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/infosas/InfosasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.infosas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
